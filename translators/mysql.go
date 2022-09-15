51c3f0ccdc09ae56984effe64e2bcd523e63bc69
Yonghwan SO <scinix@gmail.com>
2022-09-15 20:37:55 +0900

    support auto_increment for any integer types (fixes #74)

--- translators/mysql.go
+++ translators/mysql.go
@@ -224,7 +224,8 @@ func (p *MySQL) buildColumn(c fizz.Column) string {
 		s = fmt.Sprintf("%s DEFAULT %s", s, d)
 	}
 
-	if c.Primary && (c.ColType == "integer" || strings.ToLower(c.ColType) == "int") {
+	// there are also tinyint, smallint, mediumint, and bigint
+	if c.Primary && (c.ColType == "integer" || strings.HasSuffix(strings.ToLower(c.ColType), "int")) {
 		s = fmt.Sprintf("%s AUTO_INCREMENT", s)
 	}
 	return s
